dbac733c1f057e2d7c7ac7b97b01f23792ca7946
Mayank <agarwal.mayank32@gmail.com>
2017-01-21 00:40:08 +0530

    Added README.md

--- final_code_usb_android_working/final_code_usb_android_working.ino
+++ final_code_usb_android_working/final_code_usb_android_working.ino
@@ -7,7 +7,7 @@
  This example code is in the public domain.
  */
 
-int led = 10;           // the pin that the LED is attached to
+int led = 13;           // the pin that the LED is attached to or 13 for internal pin of MEGA 2560
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 int incomingByte = 0;   // for incoming serial data
